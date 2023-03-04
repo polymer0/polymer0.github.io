3eb7ec1f97bf71eca4b4446a6fdc8c74b5db11c0
Gerry Leng <114028212+klqd@users.noreply.github.com>
2023-03-04 11:38:06 +0000

    v0

---
+++ js/form.js
@@ -0,0 +1,10 @@
+function sub() {
+	let frm = document.forms['contactus'];
+	var fullname = frm['fullname'].value
+	var email = frm['email'].value
+	var msg = frm['message'].value
+	var req = "Name: " + fullname + "%0A%0AEmail: " + email + "%0A%0AMessage: " + msg
+	var url = "#"+req
+	fetch(url)
+	alert("Message sent!")
+}
